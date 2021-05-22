91899ad2c8489acad15f599c63184aa374cb2204
yastel <yasteru.kk@gmail.com>
2021-05-22 22:01:11 +0900

    fix html and add /add, /delete

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError, http::header, post};
 use askama::Template;
 use thiserror::Error;
 use r2d2_sqlite::SqliteConnectionManager;
 use r2d2::Pool;
 use rusqlite::params;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
 
 struct TodoEntry {
     id: u32,
@@ -56,6 +67,30 @@ async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpRespo
         .body(response_body))
 }
 
+#[post("/add")]
+async fn add_todo(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO todo (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
+#[post("/delete")]
+async fn delete_todo(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM todo WHERE id=?", &[&params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
     let manager = SqliteConnectionManager::file("todo.db");
@@ -71,7 +106,13 @@ async fn main() -> Result<(), actix_web::Error> {
         params![],
     )
         .expect("Failed to create a table `todo`.");
-    HttpServer::new(move || App::new().service(index).data(pool.clone()))
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_todo)
+            .service(delete_todo)
+            .data(pool.clone())
+    })
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
